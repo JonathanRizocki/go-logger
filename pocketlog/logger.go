2e5105e998ad84507e5a2ea6400453cf0a6055d9
JonathanRizocki <j.rizocki@gmail.com>
2024-04-04 17:26:40 -0700

    Expanded functionality of logger. Added tests.

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -1,37 +1,74 @@
 package pocketlog
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // New returns you a logger, ready to log at the required threshold.
-func New(threshold Level) *Logger {
-	return &Logger{
-		threshold: threshold,
+func New(threshold Level, opts ...Option) *Logger {
+	lgr := &Logger{threshold: threshold, output: os.Stdout}
+
+	for _, configFunc := range opts {
+		configFunc(lgr)
 	}
+
+	return lgr
 }
 
 // DebugF formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
+	if l.threshold > LevelDebug {
+		return
+	}
 
+	l.logf(format, args...)
 }
 
 // Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
+	if l.threshold > LevelInfo {
+		return
+	}
 
+	l.logf(format, args...)
 }
 
 // Warnf formats and prints a message if the log level is warn or higher.
 func (l *Logger) Warnf(format string, args ...any) {
+	if l.threshold > LevelWarn {
+		return
+	}
 
+	l.logf(format, args...)
 }
 
-// ErrorF formats and prints a message if the log level is error or higher
+// ErrorF formats and prints a message if the log level is error or higher.
 func (l *Logger) Errorf(format string, args ...any) {
+	if l.threshold > LevelError {
+		return
+	}
 
+	l.logf(format, args...)
 }
 
-// Fatalf formats and prints a message if the log level is fatal or higher
+// Fatalf formats and prints a message if the log level is fatal or higher.
 func (l *Logger) Fatalf(format string, args ...any) {
+	if l.threshold > LevelFatal {
+		return
+	}
 
+	l.logf(format, args...)
+}
+
+// logf prints the message to the output.
+// Add decorations here, if any.
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
